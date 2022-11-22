f945b24ff2ee39eacc4422cfc608804ea67ced5c
APeters143 <anthonypeters@delta.edu>
2022-11-21 20:57:41 -0500

    Removed validation in client, Finishied most of Login method,Needs more work on InterfaceServer, Changed login.txt from ""/t" to "-".

--- src/InterfaceServer/InterfaceServer.java
+++ src/InterfaceServer/InterfaceServer.java
@@ -11,18 +11,24 @@ public class InterfaceServer {
     public static void main(String[] args) throws FileNotFoundException {
         // Initialize variables
         int serverPort = 3000;
-        int loginKey = 4;
+        int loginKey;
         byte[] dataBuffer = new byte[65535];
-        String loginFileName = "login.txt";
+        boolean loginValid = false;
+//        String loginFileName = "login.txt";
         DatagramPacket clientPacket, DataServerPacket;
         DatagramSocket interfaceSocket;
         LinkedList<LoginNode> loginNodeLinkedList;
 
+        HandleInterface userThread;
+
         // Initialize database from Content Root
         loginNodeLinkedList = initializeFromFilePath("src/InterfaceServer/data/login.txt");
 
+        // Set loginKey to default (4)
+        loginKey = 4;
+
         try {
-            // Set up connection with client
+            // Set up socket connection with server port
             interfaceSocket = new DatagramSocket(serverPort);
 
             // Start thread loop
@@ -37,14 +43,42 @@ public class InterfaceServer {
                 // Output busy message
                 System.out.println("Connection busy");
 
-                // Create and run new thread
-                new
+                // Loop till login input is valid
+                do {
+                    // Create and run login on user thread
+                    userThread = new HandleInterface(clientPacket, loginKey, interfaceSocket, loginNodeLinkedList);
+
+                    // Check login input validation and set accountIndex
+                    loginValid = userThread.login();
+
+                    if (!loginValid) {
+                        // Input invalid
+                        // Reset buffer
+
+                        // Send back invalidation conformation to client
+
+                        // Wait for new input
+                    }
+
+                } while (!loginValid);
+
+                // Send back conformation of input
+
+                // Send list of items back to client
+
+                // Wait for selection
+
+                // Create receipt
+
+                // Wait for encrypted creditData
+
+                // Forward encrypted creditData to DataServer
+
+                // Forward conformation of creditData to client
 
                 // reset buffer for next request
 
-
             }
-
         } catch (Exception exception) {}
 
 
@@ -54,7 +88,7 @@ public class InterfaceServer {
     /**
      * Method: initializeFromFilePath, Will create a file object using given file name and LinkedList object for
      * LoginNodes. Creates scanner that will input from file to create LoginNode objects. Will separate file input by
-     * "\t" characters and fill LoginNode objects with username, password, key to populate LinkedList. When finished
+     * "-" characters and fill LoginNode objects with username, password, key to populate LinkedList. When finished
      * will return populated LinkedList of LoginNodes.
      *
      * @param inputFilePath String, path of file
@@ -72,12 +106,12 @@ public class InterfaceServer {
         // Input data from txt file to Linked list of dictionaryEntry objects
         while (fileScanner.hasNext()){
             // Input new line of test and split by tab chars
-            String[] splitInput = fileScanner.nextLine().split("\t");
+            String[] splitInput = fileScanner.nextLine().split("-");
 
             // Add LoginNode including username, password, and key to LinkedList
             linkedList.add(new LoginNode(splitInput[0].trim(),  // username
                     splitInput[1].trim(),                       // password
-                    Integer.parseInt(splitInput[2].trim()));    // key
+                    Integer.parseInt(splitInput[2].trim())));    // key
         }
 
         // Returns data collection to main
